d0c9bff395308bae971e5ce809bed936c2286ca1
Shabiq Ghazi Arkaan <shabiq.ga@bts.id>
2025-11-27 13:11:04 +0700

    adjust: home page adjustment

--- dist/header.js
+++ dist/header.js
@@ -1,6 +1,7 @@
 const header = document.querySelector("header");
 const featured = document.querySelector("#featured");
 const navBrand = document.querySelector("#nav-brand");
+const menuItems = document.querySelectorAll(".menu-item");
 let prevPosition = header.style.position;
 
 window.addEventListener("scroll", () => {
@@ -17,6 +18,9 @@ window.addEventListener("scroll", () => {
     header.style.color = "#fff";
     navBrand.style.color = "#fff";
     header.style.top = "-80px";
+    menuItems.forEach((item) => {
+      item.style.color = "#fff";
+    });
   } else {
     prevPosition = "fixed";
     header.style.top = "0px";
@@ -25,6 +29,9 @@ window.addEventListener("scroll", () => {
     header.style.color = "#0E8388";
     header.style.boxShadow = "1px 1px 10px -3px #00000033";
     navBrand.style.color = "#0E8388";
+    menuItems.forEach((item) => {
+      item.style.color = "#0E8388";
+    });
   }
 });
 
